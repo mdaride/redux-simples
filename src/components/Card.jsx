a1ae7b7061de04dd210886cdd289be7d79a9769c
mdaride <mdaride@yahoo.com.br>
2020-10-01 12:49:31 -0300

    Problema no align-items:stretch;

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import './Card.css'
+import React from 'react'
+
+export default props => {
+    return (
+        <div>
+            <div className="Card">
+                <div className="Header">
+                    <span className="Title">{props.title}</span>
+                </div>
+                <div className="Content">
+                    {props.children}
+                </div>
+            </div>
+            
+        </div>
+    )
+}
